605984422e605617bbdb67c3645dad9fa110b4ec
Sam Zanca <sam@zanca.dev>
2026-07-13 21:17:34 -0400

    fix: remove await from non-async get_merged_posts call in server function

--- src/server_functions.rs
+++ src/server_functions.rs
@@ -4,5 +4,5 @@ use crate::blog;
 
 #[post("/api/blog/posts")]
 pub async fn get_blog_posts() -> Result<Vec<blog::BlogPost>, ServerFnError> {
-    Ok(blog::get_merged_posts().await)
+    Ok(blog::get_merged_posts())
 }
